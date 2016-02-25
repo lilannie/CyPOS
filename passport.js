e2e71064652d46c00f0679fdd7302f194f167701
Jens Petersen <jens@iastate.edu>
2016-02-25 10:55:20 -0600

    Modified passport.js file

--- passport.js
+++ passport.js
@@ -1,6 +1,15 @@
 var LocalStrategy = require('passport-local').Strategy;
     express = require('express');
+    mysql = require('mysql';)
     passport = require('passport');
+    config = {
+        host: 'mysql.cs.iastate.edu',  // your host
+        user: 'dbu309grp17', // your database user
+        password: 'AugtUmP22JP', // your database password
+        database: 'db309grp17',
+        charset: 'UTF8_GENERAL_CI'
+    };
+
 
 app.use(passport.initilize(());
 app.use(passport.session()));
@@ -8,3 +17,11 @@ app.use(passport.session()));
 module.exports = function (passport, repository){
 
 }
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
