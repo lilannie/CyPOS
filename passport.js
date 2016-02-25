63e7537f5d255f1e297c6d8b1652f2aeed9a1057
Jens Petersen <jens@iastate.edu>
2016-02-25 10:50:21 -0600

    Modified passport.js file

--- passport.js
+++ passport.js
@@ -1,4 +1,9 @@
-var LocalStrategy = require('passport-local').Strategy;5
+var LocalStrategy = require('passport-local').Strategy;
+    express = require('express');
+    passport = require('passport');
+
+app.use(passport.initilize(());
+app.use(passport.session()));
 
 module.exports = function (passport, repository){
 
