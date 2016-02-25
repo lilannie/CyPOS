d6bf993f5c949e0e08ffcb8774eff59fa1525b5a
Annie Steenson <anniesteenso@gmail.com>
2016-02-24 20:19:41 -0600

    issue with loading external js file?

---
+++ routes.js
@@ -0,0 +1,48 @@
+var router = require('express').Router()
+var express = require('express');
+var path = __dirname + '/public/views/';
+
+// route middleware that will happen on every request
+router.use(function(req, res, next) {
+    // log each request to the console
+    console.log(req.method, req.url);
+    // continue doing what we were doing and go to the route
+    next();
+});
+
+/*Basic Routes*/
+router.get("/",function(req,res){
+    res.sendFile(path + "index.html");
+});
+router.get("/home",function(req,res){
+    res.sendFile(path + "home.html");
+});
+router.get("/classes",function(req,res){
+    res.sendFile(path + "classes.html");
+});
+router.get("/help",function(req,res){
+    res.sendFile(path + "help.html");
+});
+router.get("/history",function(req,res){
+    res.sendFile(path + "history.html");
+});
+router.get("/home",function(req,res){
+    res.sendFile(path + "home.html");
+});
+router.get("/manage",function(req,res){
+    res.sendFile(path + "manage.html");
+});
+router.get("/myaccount",function(req,res){
+    res.sendFile(path + "myaccount.html");
+});
+router.get("/new",function(req,res){
+    res.sendFile(path + "new.html");
+});
+router.get("/view",function(req,res){
+    res.sendFile(path + "view.html");
+});
+
+//router.use(express.static(__dirname + '/public'));
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,33 +1,18 @@
+/*Dependency Variables*/
 var express = require('express');
-var pdf2json = require('pdf2json');
+/*var pdf2json = require('pdf2json');*/
 var router = express.Router();
-
-var path = __dirname + '/public/views/';
 var app = express();
+
+/*Variables to make things easier to read*/
 var port = 8080;
+var path = __dirname + '/public/views/';
 
-router.get("/",function(req,res){
-    res.sendFile(path + "index.html");
-});
-
-router.get(/home/,function(req,res){
-    res.sendFile(path + "home.html");
-});
-
-router.get("/new",function(req,res){
-    res.sendFile(path + "new.html");
-});
-
-app.use("/",router);
-app.use(express.static('public'));
-
-/*//Defining middleware to server static files
-app.use('static', express.static('./node_modules/bootstrap'));*/
-
-app.use("*",function(req,res){
+/*Express App Routing*/
+app.use("/",require('./routes'));
+/*app.use("*",function(req,res){
     res.sendFile(path + "404.html");
-});
-
+});*/
 app.listen(port, function () {
     console.log('listening on port '+ port);
 });
