972618f95e8fa516ad3e259b9b9a8ef0ba107452
Jens Petersen <jens@iastate.edu>
2016-02-28 17:45:20 -0600

    made lots of changes to passport related files

--- server.js
+++ server.js
@@ -1,15 +1,48 @@
 // vendor library
 var passport = require('passport');
 var bcrypt = require('bcrypt-nodejs');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var ejs = require('ejs');
+var LocalStrategy = require('passport-local').Strategy;
 
 // custom library
 // model
 var Model = require('./database/model');
+var functions = require('./functions');
 
 var express = require('express');
 /*var pdf2json = require('pdf2json');*/
 var router = express.Router();
 var app = express();
+
+passport.use(new LocalStrategy(function(username, password, done) {
+    new Model.User({username: username}).fetch().then(function(data) {
+        var user = data;
+        if(user === null) {
+            return done(null, false, {message: 'Invalid username or password'});
+        } else {
+            user = data.toJSON();
+            if(!bcrypt.compareSync(password, user.password)) {
+                return done(null, false, {message: 'Invalid username or password'});
+            } else {
+                return done(null, user);
+            }
+        }
+    });
+}));
+
+passport.serializeUser(function(user, done) {
+    done(null, user.username);
+});
+
+passport.deserializeUser(function(username, done) {
+    new Model.User({username: username}).fetch().then(function(user) {
+        done(null, user);
+    });
+});
+
 var dbConfig = {
     client: 'mysql',
     connection: {
@@ -20,10 +53,15 @@ var dbConfig = {
         charset: 'utf8'
     }
 };
-app.set('bookshelf', bookshelf);
 
-var knex = require('knex')(dbConfig);
-var bookshelf = require('bookshelf')(knex);
+//var knex = require('knex')(dbConfig);
+//var bookshelf = require('bookshelf')(knex);
+//app.set('bookshelf', bookshelf);
+app.use(cookieParser());
+app.use(bodyParser());
+app.use(session({secret: 'secret strategic xxzzz code'}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 /*Variables to make things easier to read*/
 var port = 8080;
@@ -38,3 +76,4 @@ app.use("*",function(req,res){
 app.listen(port, function () {
     console.log('listening on port '+ port);
 });
+
