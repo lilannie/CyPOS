0a493ba9c4f26c6462d3599100ba909b107d819d
Annie Steenson <anniesteenso@gmail.com>
2016-03-09 20:19:34 -0600

    Updating routing and view functions

--- Django/cypos/cypos/urls.py
+++ Django/cypos/cypos/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from base import views
+
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'cypos.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', views.index, name='index'),
+    url(r'^user/(?P<id>\d+)/', views.user_detail, name='user_detail'),
     url(r'^admin/', include(admin.site.urls)),
 ]
