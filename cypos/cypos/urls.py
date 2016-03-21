b40da0d034fefe280ef8426fcbc27ecba4f90276
Annie Steenson <anniesteenso@gmail.com>
2016-03-20 22:21:50 -0500

    Changes to routing

--- cypos/cypos/urls.py
+++ cypos/cypos/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from cypos.base import views
+from base import views
 
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^courses', views.course_view, name='course_view'),
+    url(r'^courses', views.courses_view, name='course_view'),
     url(r'^user/(?P<id>\d+)/', views.user_detail, name='user_detail'),
     url(r'^admin/', include(admin.site.urls)),
 ]
