92e1f009672ca3b1820a54df5a92c9eed99be03d
George Zachariades <yiorgos@cyprusweb.com>
2016-03-21 15:06:47 -0500

    deleting the php stuff

--- query/classes.php
+++
@@ -1,7 +0,0 @@
-<?php
-	include $_SERVER['DOCUMENT_ROOT'] . '/files/connect.php';
-	$queryString = htmlspecialchars($_GET['major']) . "%";
-	$result = mysqli_query($conn, "SELECT Courses.number, Courses.name, Courses.prereqs, Courses.description FROM Courses WHERE Courses.number LIKE '$queryString'");
-	if (!$result)
-		echo 'There was an error ' . mysqli_error($conn);
-?>
--- query/departments.php
+++
@@ -1,6 +0,0 @@
-<?php
-	include $_SERVER['DOCUMENT_ROOT'] . '/files/connect.php';
-	$result = mysqli_query($conn, "SELECT Departments.acronym, Departments.name FROM Departments");
-	if (!$result)
-		echo 'There was an error ' . mysqli_error($conn);
-?>
