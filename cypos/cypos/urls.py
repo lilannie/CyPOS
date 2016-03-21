88fc5993021ba32037972b5b3b6dbef6676f10e1
Annie Steenson <anniesteenso@gmail.com>
2016-03-21 14:47:49 -0500

    Changes to the toolbar

--- cypos/cypos/urls.py
+++ cypos/cypos/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     url(r'^home', views.home, name='home'),
     url(r'^manage', views.user_manage, name='user_manage'),
     url(r'^new', views.pos_new, name='pos_new'),
+    url(r'^user', views.user_detail, name='user_detail'),
     url(r'^user/(?P<id>\d+)/', views.user_detail, name='user_detail'),
     url(r'^view', views.pos_view, name='pos_view'),
     url(r'^admin/', include(admin.site.urls)),
