652c58e6a525f04780e537fdabb0b2e21b01ce01
Jens Petersen <jens@iastate.edu>
2016-03-25 14:55:11 -0500

    written structure for business logic

---
+++ files/Logic/schedule_creator.py
@@ -0,0 +1,35 @@
+#
+#This is the code in which the schedules will be generated with.
+#
+#!/usr/bin/python
+
+import MySQLdb
+
+class ScheduleCreator:
+
+TODO
+#Function
+##Write a function that connects to the Database
+#return connection
+def databaseConnection():
+
+TODO
+#Function
+##Write a function that queries database
+#return the query
+def databaseQuery():
+
+TODO
+#Function
+##Write a function that takes in 2 arguments
+###1. list of required classes
+###2. list of classes already taken
+##filter through removing classes which appear in both arrays/lists (type tbd)
+#return list of filtered classes
+def classFilter():
+
+TODO
+#Function
+##Write a function that checks pre-requisites
+def prereq():
+
