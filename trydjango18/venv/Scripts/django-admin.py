5cf917fc4cfbb7a75a7989615a24e8f61e13ef55
Jens Petersen <jens@iastate.edu>
2016-03-25 17:27:46 -0500

    created working django virtual environment

---
+++ trydjango18/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\users\jens\onedrive\4.spring2016\com_s_309\g17_cypos\trydjango18\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
