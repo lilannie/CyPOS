9efb04d21d507010b6b3571cd0b2a7da7be31267
Jens Petersen <jens@iastate.edu>
2016-03-26 01:30:17 -0500

    created simple printing function

---
+++ pythonPractice.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+# This file is for python practice and is not to be used in the actual project
+
+#Function that return a passed string
+def printlife(passedString):
+   print( passedString)
+   return
+
+printlife("hello world!")
+printlife("life is good")
+printlife("Samsung")
+
+
+
+
