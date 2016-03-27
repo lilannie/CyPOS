7fdd1b56770cc514c4d34c6ecd2ccfd6803b5578
Annie Steenson <anniesteenso@gmail.com>
2016-03-27 11:10:44 -0500

    Changes to scripts to populate the database

--- cypos/scripts/se_requiredclasses.py
+++ cypos/scripts/se_requiredclasses.py
@@ -1,22 +1,22 @@
 from base.models import Majors, Departments, Courses
-from django.core.exceptions import ObjectDoesNotExist
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 
 
 def run():
-    fnumbers = ['165', '167', '101', '185', '160', '150', '166', '221', '227', '166', '267', '212', '228', '281', '250',
+    numbers = ['165', '167', '101', '185', '160', '150', '166', '221', '227', '166', '267', '212', '228', '281', '250',
                 '229', '363', '319', '309', '330', '321', '339', '329', '311', '352', '314', '330', '491', '494', '492']
-    fdepartments = ['Math', 'Chem', 'SE', 'CprE', 'Lib', 'Engl', 'Math', 'Phys', 'ComS', 'SE', 'Math', 'SpCm', 'ComS',
+    departments = ['Math', 'Chem', 'SE', 'CprE', 'Lib', 'Engl', 'Math', 'Phys', 'ComS', 'SE', 'Math', 'SpCm', 'ComS',
                     'CprE', 'Engl', 'ComS',
                     'ComS', 'SE', 'ComS', 'ComS', 'ComS', 'SE', 'SE', 'ComS', 'ComS', 'Engl', 'Stat', 'SE', 'SE', 'SE']
-    se = Majors.objects.get(name="Software Engineering")
+    se = Majors.objects.get(name='Software Engineering')
     count = 0
-    for number in fnumbers:
+    for number in numbers:
         try:
-            depart = Departments.objects.get(acronym=fdepartments[count])
-            course = Courses(number=number, department_id=depart.id, acronym=fdepartments[count]+" "+number)
-            course.save()
+            course = Courses.objects.get(acronym=departments[count].upper()+" "+numbers[count])
             se.reqCourses.add(course)
         except ObjectDoesNotExist:
-            print("depart object unsuccessful: "+fdepartments[count])
+            print("depart object unsuccessful: "+numbers[count])
+        except MultipleObjectsReturned:
+            print("depart object unsuccessful: "+numbers[count])
         count += 1
     print("finished")
