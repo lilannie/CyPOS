79ecfd51bee4a203b5d65f21ac2daaebbb1c419a
Jens <jens@iastate.edu>
2016-03-29 01:10:37 -0500

    modified urls.py to allow routing to user_edit.html

--- cypos/cypos/urls.py
+++ cypos/cypos/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     url(r'^manage', views.user_manage, name='user_manage'),
     url(r'^new', views.pos_new, name='pos_new'),
     url(r'^register', views.register, name='register'),
-    url(r'^user', views.user_detail, name='user_detail'),
+    #url(r'^user', views.user_detail, name='user_detail'),
     url(r'^user/(?P<id>\d+)/', views.user_detail, name='user_detail'),
     url(r'^view', views.pos_view, name='pos_view'),
     url(r'^admin/', include(admin.site.urls)),
