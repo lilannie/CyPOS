ec5bc9c8749a6204642c34be95e17c847d4db102
Jens <jens@iastate.edu>
2016-03-29 02:08:23 -0500

    commented out redundant code?

--- cypos/base/forms.py
+++ cypos/base/forms.py
@@ -4,11 +4,11 @@ from django import forms
 
 
 class UserForm (ModelForm):
-    first_name = forms.CharField(help_text="First Name:")
-    last_name = forms.CharField(help_text="Last Name:")
-    username = forms.CharField(help_text="Username:")
-    email = forms.CharField(help_text="Email:")
-    password = forms.CharField(widget=forms.PasswordInput(), help_text="Password:")
+    # first_name = forms.CharField(help_text="First Name:")
+    # last_name = forms.CharField(help_text="Last Name:")
+    # username = forms.CharField(help_text="Username:")
+    # email = forms.CharField(help_text="Email:")
+    # password = forms.CharField(widget=forms.PasswordInput(), help_text="Password:")
 
     class Meta:
         model = User
