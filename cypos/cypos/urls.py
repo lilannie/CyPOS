e4cbf842aed99400c4d7bcceef2105a974e46f5c
Jens <jens@iastate.edu>
2016-04-05 01:09:30 -0500

    added url like to be used to generate django form at a later date

--- cypos/cypos/urls.py
+++ cypos/cypos/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     url(r'^help', views.user_help, name='user_help'),
     url(r'^history', views.pos_history, name='pos_history'),
     url(r'^home', views.home, name='home'),
-    url(r'^manage', views.user_manage, name='user_manage'),
+    url(r'^manage$', views.user_manage, name='user_manage'),
     url(r'^new', views.pos_new, name='pos_new'),
     url(r'^register', views.register, name='register'),
     #url(r'^user', views.user_detail, name='user_detail'),
@@ -21,4 +21,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
 
     url(r'^user_edit', views.user_edit, name='user_edit'),
+    #url(r'^user_edit/$', views.UserEditForm.get(), name='user-edit-page')
 ]
