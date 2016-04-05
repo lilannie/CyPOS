4dc50de0d0d42671c19fdb83a43da54bb4f36eb6
Jens <jens@iastate.edu>
2016-04-05 01:31:58 -0500

    added forms for user_edit and changePassword to allow django to eventually be able to generate forms for the user (work in progress

--- cypos/base/forms.py
+++ cypos/base/forms.py
@@ -14,4 +14,31 @@ class UserForm (ModelForm):
         model = User
         fields = ('first_name', 'last_name', 'username', 'email', 'password')
 
+# Modify user information form 
+class UserEditForm(ModelForm):
+    class Meta:
+       #model = models.Team
+        fields = ['first_name', 'last_name', 'username', 'email']
+        fieldsets = [{
+            'id': 'modify-team',
+            'legend': 'Modify User',
+            'title': "Modify your user information here. Any information you would like to keep, do not modify.",
+        }]
 
+class ChangePasswordForm(ModelForm):
+    old_password = forms.CharField(widget=forms.PasswordInput(), required=True)
+    new_password = forms.CharField(widget=forms.PasswordInput(), required=True)
+    new_password_again = forms.CharField(widget=forms.PasswordInput(), required=True)
+
+    class Meta:
+        fieldsets = [{
+            'id': 'update-password',
+            'legend': 'Change Password',
+            'title': 'Your password should be at least 8 characters long.<br>' +
+                     'Passwords must contain a lowercase letter, an uppercase letter, and a number or symbol.'
+        }]
+
+    def clean(self):
+        cleaned_data = super(ChangePasswordForm, self).clean()
+        if cleaned_data.get('new_password') != cleaned_data.get('new_password_again'):
+            raise forms.ValidationError("The passwords you inputted do not match.")
