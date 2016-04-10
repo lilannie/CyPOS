2bdf46c0f373d65fa1c5711b1f9824ab56861f0f
Jens <jens@iastate.edu>
2016-04-10 15:55:23 -0500

    Deleting test project, Jens made

--- trydjango18/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!d:\users\jens\onedrive\4.spring2016\com_s_309\g17_cypos\trydjango18\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
