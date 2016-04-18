f66bbea8d55831f4eacee7de05f62c3a15994a20
Jens <jens@iastate.edu>
2016-04-18 12:52:14 -0500

    formatting change

--- cypos/cypos/settings.py
+++ cypos/cypos/settings.py
@@ -88,6 +88,7 @@ DATABASES = {
 
     }
 }
+
 # Documentation found at: 
 # http://django-haystack.readthedocs.org/en/v2.4.1/tutorial.html#installation
 # http://django-haystack.readthedocs.org/en/v2.4.1/settings.html
